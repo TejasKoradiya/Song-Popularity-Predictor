ef00d51e9c3a198e0a8cb538cac4cadd96a7c776
Parth Movaliya <parthmovaliya55055@gmail.com>
2023-04-21 21:39:37 +0530

    Adding Navbar

---
+++ index.js
@@ -0,0 +1,10 @@
+// =============Navbar=================
+var navbar = document.getElementById("navBar");
+var btns = navbar.getElementsByClassName("navbar_btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
